16d9fd48ad6cb7553c4253432799ed43c6eaa2ad
kapawit <wahib05jhf@gmail.com>
2023-05-20 09:25:16 +0700

    fix username db

--- tugas/pertemuan11/koneksi.php
+++ tugas/pertemuan11/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $servername1 = "localhost";
-$username1 = "root";
-$password1 = "";
+$username1 = "pemrograman";
+$password1 = "LASJD*ASDUOIYO qoyiwyioOIASHDO OIH";
 $conn1 = new mysqli($servername1, $username1, $password1);
 if ($conn1->connect_error) { die("Connection failed: " . $conn1->connect_error);
     
