c301d5dce0660ff2c9bb3fc8051403fb2c8ed428
kapawit <wahib05jhf@gmail.com>
2023-05-20 16:57:52 +0700

    add tugas pertemuan 12

--- tugas/pertemuan12.php
+++ tugas/pertemuan12.php
@@ -19,26 +19,3 @@
         <?php } ?>
     <?php } ?>
   </div>
-
-  <div class="card p-4 mt-4">
-  <h4>Latihan Soal</h4>
-<?php
-    $dir = "tugas/pertemuan11_latihansoal";
-    $files = scandir($dir);
-?>
-<?php foreach($files as $file) {
-    if(substr($file, -4) === ".php") {
-        if (isset($_GET['page'])) {
-          $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
-        }
-      ?>
-        <a class="list-group-item hvr-forward list-group-item-action" href="<?php echo 'tugas/pertemuan12_latihansoal/' . $file; ?>">
-          <span class="d-flex justify-content-between">
-            <?php echo $file; ?>
-            <i class="bi bi-arrow-right"></i>
-          </span>
-        </a>
-        <?php } ?>
-    <?php } ?>
-  </div>
-</div>
