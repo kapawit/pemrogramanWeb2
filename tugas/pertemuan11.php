a9af7308489ba2ef6ca4d07738c7e8dc8da67125
kapawit <wahib05jhf@gmail.com>
2023-05-20 09:49:53 +0700

    fix pertemuan 11

--- tugas/pertemuan11.php
+++ tugas/pertemuan11.php
@@ -1,11 +1,31 @@
 <div class="card p-4">
 <h4>KONEKSI DATABASE, INSERT DAN SELECT</h4>
 <?php
-    $dir = "tugas/pertemuan11";
+    $dir = "tugas/pertemuan_11";
     $files = scandir($dir);
 ?>
+<?php foreach($files as $file) {
+    if(substr($file, -4) === ".php") {
+        if (isset($_GET['page'])) {
+          $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
+        }
+      ?>
+        <a class="list-group-item hvr-forward list-group-item-action" href="<?php echo 'tugas/pertemuan11/' . $file; ?>">
+          <span class="d-flex justify-content-between">
+            <?php echo $file; ?>
+            <i class="bi bi-arrow-right"></i>
+          </span>
+        </a>
+        <?php } ?>
+    <?php } ?>
+  </div>
 
-
+  <div class="card p-4 mt-4">
+  <h4>MANIPULASI DATABASE UPDATE DAN DELETE</h4>
+<?php
+    $dir = "tugas/pertemuan11_latihansoal";
+    $files = scandir($dir);
+?>
 <?php foreach($files as $file) {
     if(substr($file, -4) === ".php") {
         if (isset($_GET['page'])) {
