7383d2e35d256ca12b3c22d2cdc3f37a3ba7071b
kapawit <wahib05jhf@gmail.com>
2023-05-26 14:54:22 +0700

    fix tampilan

--- asset/script.js
+++ asset/script.js
@@ -17,3 +17,19 @@ toggleSwitch.addEventListener('change', function() {
   htmlElement.setAttribute('data-bs-theme', theme);
 });
 
+(function(){
+  var watermark = document.createElement("div");
+  watermark.innerHTML = "Pawit wahib";
+  var watermarkStyle = watermark.style;
+  watermarkStyle.position = "fixed";
+  watermarkStyle.bottom = "20px";
+  watermarkStyle.right = "20px";
+  watermarkStyle.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  watermarkStyle.color = "#fff";
+  watermarkStyle.fontFamily = "Arial";
+  watermarkStyle.fontSize = "14px";
+  watermarkStyle.padding = "5px";
+  watermarkStyle.zIndex = "9999";
+
+  document.body.appendChild(watermark);
+})();
