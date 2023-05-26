5bdedcde2854da65d51ffcc677daf246e1e68c10
kapawit <wahib05jhf@gmail.com>
2023-05-26 13:50:54 +0700

    add perteemuan 13 latihan soal

---
+++ tugas/pertemuan13_latihansoal/cari.php
@@ -0,0 +1,27 @@
+<?php 
+$koneksi=mysqli_connect("localhost","root","","db_latihan13");
+$cari=$_POST['nama'];
+$hasil=mysqli_query($koneksi,"select * from tabel_mahasiswa where jurusan like '%$cari%' order by jurusan asc"); 
+
+echo
+"<html>
+<body>
+<table>
+<tr>
+<th>NIM</th>
+<th>nama</th>
+<th>alamat</th>
+<th>jurusan</th>
+</tr>";
+While($data=mysqli_fetch_array($hasil)){
+    echo "<tr>
+    <td>".$data['nim']."</td>
+    <td>".$data['nama']."</td>
+    <td>".$data['alamat']."</td>
+    <td>".$data['jurusan']."</td>
+    </tr>
+    </body>
+    </html>";
+    }
+?>
+    
---
+++ tugas/pertemuan13_latihansoal/formsearch.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h3>Mencari record berdasarkan field jurusan<h3>
+<form action="cari.php" method="post">
+<label>Maukan nama : </label><input type="text" name="jurusan"><br>
+<p><input type="submit" value="Search">	<input type="reset" value="Hapus"></p>
+</form>
+</h3>
+</body>
+</html>
